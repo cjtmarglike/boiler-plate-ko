70cf033c0a7bb4192702730231766a340d79a3f5
k <cjtmarglike@naver.com>
2022-02-22 15:22:06 +0900

    security

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const config = require('./config/key')
+const { User } = require('./models/users');
+
+//application/x-www-form-urlencoded로 돼있는 것을 가져올 수 있게 해줌
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json으로 돼있는 것을 가져올 수 있게 해줌
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://cjtmarglike:doremi@cluster0.p1zzc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
     .then(()=> console.log('MongoDB connected...'))
     .catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요')
+  res.send('Hello World! 안녕하세요dd')
+})
+
+app.post('/register', (req, res) => {
+
+  //1. get info from client for sign up
+  const user = new User(req.body)
+  //2. insert into database
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
 app.listen(port, () => {
